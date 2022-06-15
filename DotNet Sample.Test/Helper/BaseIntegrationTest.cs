b2b17b3d8c74725481a6ca8bac2c854c5ff47c3f
rudine <rudinesurya@gmail.com>
2022-06-15 18:14:31 +0100

    fixed inmemorydb not resetting issues inbetween tests

--- DotNet Sample.Test/Helper/BaseIntegrationTest.cs
+++ DotNet Sample.Test/Helper/BaseIntegrationTest.cs
@@ -1,10 +1,4 @@
-﻿using DotNet_Sample.Data;
-using Microsoft.AspNetCore.Mvc.Testing;
-using Microsoft.AspNetCore.TestHost;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using System;
-using System.Linq;
+﻿using System;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
@@ -33,29 +27,20 @@ namespace DotNet_Sample.Test.Helper
 
     public abstract class BaseIntegrationTest : IDisposable
     {
-        protected readonly HttpClient TestClient;
+        TestApp App;
+        public HttpClient TestClient;
 
         public BaseIntegrationTest()
         {
-            var appFactory = new WebApplicationFactory<Program>()
-                .WithWebHostBuilder(builder =>
-                {
-                    builder.ConfigureTestServices(services =>
-                    {
-                        var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<AppDbContext>));
-                        if (descriptor != null)
-                            services.Remove(descriptor);
+            App = new TestApp();
 
-                        services.AddDbContext<AppDbContext>(options => options.UseInMemoryDatabase(databaseName: "InMemoryDbForTesting"));
-                    });
-                });
-
-            TestClient = appFactory.CreateClient();
+            TestClient = App.CreateClient();
         }
 
         public void Dispose()
         {
             TestClient.Dispose();
+            App.Dispose();
         }
     }
 }
---
+++ DotNet Sample.Test/Helper/TestApp.cs
@@ -0,0 +1,40 @@
+﻿using DotNet_Sample.Data;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace DotNet_Sample.Test.Helper
+{
+    public class TestApp : WebApplicationFactory<Program>
+    {
+        private readonly string _environment;
+
+        public TestApp(string environment = "Test")
+        {
+            _environment = environment;
+        }
+
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            builder.UseEnvironment(_environment);
+
+            var databaseName = "testdb" + Guid.NewGuid().ToString();
+
+            // Add mock/test services to the builder here
+            builder.ConfigureServices(services =>
+            {
+                services.AddScoped(sp =>
+                {
+                    return new DbContextOptionsBuilder<AppDbContext>()
+                        .UseInMemoryDatabase(databaseName)
+                        .UseApplicationServiceProvider(sp)
+                        .Options;
+                });
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
